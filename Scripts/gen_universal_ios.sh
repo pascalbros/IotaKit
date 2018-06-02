48574632ebdb301cceb702cc96b415337498f2a3
Pasquale Ambrosini <pasquale.ambrosini@gmail.com>
2018-06-02 21:06:46 +0200

    Fixed universal script

--- Scripts/gen_universal_ios.sh
+++ Scripts/gen_universal_ios.sh
@@ -1,48 +1,29 @@
 #!/bin/sh
 
-# Instructions:
-# 1. Select "IotaKit-Package" scheme
-# 2. Select "Simulator" and build
-# 3. Select "Generic iOS Device" and build
-# 4. Select Universal scheme (Generic iOS Device) and build
-# 5. The lib will be copied to ~/Desktop/Frameworks/
 set -e
 
 FRAMEWORK=IotaKit
 
-BUILD=~/.xcodebuild/$FRAMEWORK/build
+BUILD=build
 FRAMEWORK_PATH=$FRAMEWORK.framework
+OUTPUT_PATH=${HOME}/Desktop/Frameworks
+cd ..
 
 rm -Rf $BUILD
+rm -f $FRAMEWORK.framework.tar.gz
 
-if [[ "$SDK_NAME" =~ ([A-Za-z]+) ]]; then
-RW_SDK_PLATFORM=${BASH_REMATCH[1]}
-else
-echo "Could not find platform name from SDK_NAME: $SDK_NAME"
-exit 1
-fi
+xcodebuild archive -project $FRAMEWORK.xcodeproj -scheme $FRAMEWORK-Package -sdk iphoneos SYMROOT=$BUILD
+xcodebuild build -configuration Release -project $FRAMEWORK.xcodeproj -target $FRAMEWORK -sdk iphonesimulator SYMROOT=$BUILD
 
-# 3 - Determine the other platform
-if [ "$RW_SDK_PLATFORM" == "iphoneos" ]; then
-RW_OTHER_PLATFORM=iphonesimulator
-else
-RW_OTHER_PLATFORM=iphoneos
-fi
+cp -RL $BUILD/Release-iphoneos $BUILD/Release-universal
+cp -RL $BUILD/Release-iphonesimulator/$FRAMEWORK_PATH/Modules/$FRAMEWORK.swiftmodule/* $BUILD/Release-universal/$FRAMEWORK_PATH/Modules/$FRAMEWORK.swiftmodule
 
-# 4 - Find the build directory
-if [[ "$BUILT_PRODUCTS_DIR" =~ (.*)$RW_SDK_PLATFORM$ ]]; then
-RW_OTHER_BUILT_PRODUCTS_DIR="${BASH_REMATCH[1]}${RW_OTHER_PLATFORM}"
-else
-echo "Could not find other platform build directory."
-exit 1
-fi
+lipo -create $BUILD/Release-iphoneos/$FRAMEWORK_PATH/$FRAMEWORK $BUILD/Release-iphonesimulator/$FRAMEWORK_PATH/$FRAMEWORK -output $BUILD/Release-universal/$FRAMEWORK_PATH/$FRAMEWORK
 
-mkdir -p "$BUILD/Release-universal/"
-cp -RL "$BUILT_PRODUCTS_DIR/" "$BUILD/Release-universal"
-cp -R "$RW_OTHER_BUILT_PRODUCTS_DIR/$FRAMEWORK_PATH/Modules/$FRAMEWORK.swiftmodule/" "$BUILD/Release-universal/$FRAMEWORK_PATH/Modules/$FRAMEWORK.swiftmodule"
-lipo -create "$BUILT_PRODUCTS_DIR/$FRAMEWORK_PATH/$FRAMEWORK" "$RW_OTHER_BUILT_PRODUCTS_DIR/$FRAMEWORK_PATH/$FRAMEWORK" -output "$BUILD/Release-universal/$FRAMEWORK_PATH/$FRAMEWORK"
+tar -czv -C $BUILD/Release-universal -f $FRAMEWORK.tar.gz $FRAMEWORK_PATH $FRAMEWORK_PATH.dSYM
 
-mkdir -p "${HOME}/Desktop/Frameworks/"
-rm -rf "${HOME}/Desktop/Frameworks/$FRAMEWORK.framework"
-cp -R "$BUILD/Release-universal/$FRAMEWORK_PATH" "${HOME}/Desktop/Frameworks/$FRAMEWORK.framework"
-open "${HOME}/Desktop/Frameworks/"
+mkdir -p $OUTPUT_PATH
+rm -rf $OUTPUT_PATH/$FRAMEWORK
+cp -R $BUILD/Release-universal/ $OUTPUT_PATH/$FRAMEWORK
+rm -Rf $BUILD
+open $OUTPUT_PATH
