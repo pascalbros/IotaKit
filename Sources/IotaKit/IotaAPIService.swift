8c65beea5b59a4808ee83e077c7ad3cb10022e22
Pasquale Ambrosini <pasquale.ambrosini@gmail.com>
2020-07-05 17:23:25 +0200

    Improved IotaAPIService

--- Sources/IotaKit/IotaAPIService.swift
+++ Sources/IotaKit/IotaAPIService.swift
@@ -9,10 +9,10 @@ import Foundation
 
 /// Carries an error message.
 public struct IotaAPIError: Error {
-	
+
 	/// The error message.
 	public let message: String
-	
+
 	/// The initializer for IotaAPIError.
 	///
 	/// - Parameter message: The error message.
@@ -22,37 +22,41 @@ public struct IotaAPIError: Error {
 }
 
 class IotaAPIService: IotaAPIServices {
-	
+
 	fileprivate static let service: WebServices.Type = PAWSRequest.self
-	
+
 	fileprivate static func command(withString command: String) -> [String: Any] {
 		return ["command": command]
 	}
-	
+
 	fileprivate static let defaultTimeout = 60
-	
+
 	static func nodeInfo(nodeAddress: String, _ success: @escaping ([String: Any]) -> Void, _ error: @escaping (Error) -> Void) {
-		
+
 		let data = command(withString: "getNodeInfo")
-	
-		service.POST(data: data, destination: nodeAddress, timeout: 3, successHandler: { (r) in
-			guard let dict = r.jsonToObject() as? [String: Any] else {
+
+		service.POST(data: data, destination: nodeAddress, timeout: 3, successHandler: { result in
+			guard let dict = result.jsonToObject() as? [String: Any] else {
 				error(IotaAPIError("Error converting JSON"))
 				return
 			}
 			success(dict)
-		}) { (e) in
-			error(e)
-		}
+		}, errorHandler: { err in
+			error(err)
+		})
 	}
-	
-	static func balances(nodeAddress: String, addresses: [String], _ success: @escaping (_ balances: [String: Int64]) -> Void, _ error: @escaping (Error) -> Void) {
-		
+
+	static func balances(
+		nodeAddress: String,
+		addresses: [String],
+		_ success: @escaping (_ balances: [String: Int64]) -> Void,
+		_ error: @escaping (Error) -> Void) {
+
 		var data = command(withString: "getBalances")
 		data["addresses"] = addresses
 		data["threshold"] = 100
-		service.POST(data: data, destination: nodeAddress, timeout: defaultTimeout, successHandler: { (r) in
-			guard let dict = r.jsonToObject() as? [String: Any] else {
+		service.POST(data: data, destination: nodeAddress, timeout: defaultTimeout, successHandler: { result in
+			guard let dict = result.jsonToObject() as? [String: Any] else {
 				error(IotaAPIError("Error converting JSON"))
 				return
 			}
@@ -60,23 +64,28 @@ class IotaAPIService: IotaAPIServices {
 				error(IotaAPIError("Error retrieving hashes"))
 				return
 			}
-			
+
 			var result: [String: Int64] = [:]
 			for i in 0..<balances.count {
 				result[addresses[i]] = Int64(balances[i])!
 			}
 			success(result)
-		}) { (e) in
-			error(e)
-		}
+		}, errorHandler: { err in
+			error(err)
+		})
 	}
-	
-	static func findTransactions(nodeAddress: String, type: IotaFindTxType, query: [String], _ success: @escaping (_ hashes: [String]) -> Void, _ error: @escaping (Error) -> Void) {
-		
+
+	static func findTransactions(
+		nodeAddress: String,
+		type: IotaFindTxType,
+		query: [String],
+		_ success: @escaping (_ hashes: [String]) -> Void,
+		_ error: @escaping (Error) -> Void) {
+
 		var data = command(withString: "findTransactions")
 		data[type.rawValue] = query
-		service.POST(data: data, destination: nodeAddress, timeout: defaultTimeout, successHandler: { (r) in
-			guard let dict = r.jsonToObject() as? [String: Any] else {
+		service.POST(data: data, destination: nodeAddress, timeout: defaultTimeout, successHandler: { result in
+			guard let dict = result.jsonToObject() as? [String: Any] else {
 				error(IotaAPIError("Error converting JSON"))
 				return
 			}
@@ -85,17 +94,21 @@ class IotaAPIService: IotaAPIServices {
 				return
 			}
 			success(hashes)
-		}) { (e) in
-			error(e)
-		}
+		},errorHandler: { err in
+			error(err)
+		})
 	}
-	
-	static func trytes(nodeAddress: String, hashes: [String], _ success: @escaping (_ trytes: [IotaTransaction]) -> Void, _ error: @escaping (Error) -> Void) {
-		
+
+	static func trytes(
+		nodeAddress: String,
+		hashes: [String],
+		_ success: @escaping (_ trytes: [IotaTransaction]) -> Void,
+		_ error: @escaping (Error) -> Void) {
+
 		var data = command(withString: "getTrytes")
 		data["hashes"] = hashes
-		service.POST(data: data, destination: nodeAddress, timeout: defaultTimeout, successHandler: { (r) in
-			guard let dict = r.jsonToObject() as? [String: Any] else {
+		service.POST(data: data, destination: nodeAddress, timeout: defaultTimeout, successHandler: { result in
+			guard let dict = result.jsonToObject() as? [String: Any] else {
 				error(IotaAPIError("Error converting JSON"))
 				return
 			}
@@ -108,20 +121,27 @@ class IotaAPIService: IotaAPIServices {
 				result.append(IotaTransaction(trytes: trytes[i]))
 			}
 			success(result)
-		}) { (e) in
-			error(e)
-		}
+		}, errorHandler: { err in
+			error(err)
+		})
 	}
-	
-	static func attachToTangle(nodeAddress: String, trunkTx: String, branchTx: String, minWeightMagnitude: Int = IotaConstants.mwm, trytes: [String], _ success: @escaping (_ trytes: [String]) -> Void, _ error: @escaping (Error) -> Void) {
-		
+
+	static func attachToTangle(
+		nodeAddress: String,
+		trunkTx: String,
+		branchTx: String,
+		minWeightMagnitude: Int = IotaConstants.mwm,
+		trytes: [String],
+		_ success: @escaping (_ trytes: [String]) -> Void,
+		_ error: @escaping (Error) -> Void) {
+
 		var data = command(withString: "attachToTangle")
 		data["trunkTransaction"] = trunkTx
 		data["branchTransaction"] = branchTx
 		data["minWeightMagnitude"] = minWeightMagnitude
 		data["trytes"] = trytes
-		service.POST(data: data, destination: nodeAddress, timeout: defaultTimeout, successHandler: { (r) in
-			guard let dict = r.jsonToObject() as? [String: Any] else {
+		service.POST(data: data, destination: nodeAddress, timeout: defaultTimeout, successHandler: { result in
+			guard let dict = result.jsonToObject() as? [String: Any] else {
 				error(IotaAPIError("Error converting JSON"))
 				return
 			}
@@ -130,20 +150,25 @@ class IotaAPIService: IotaAPIServices {
 				return
 			}
 			success(trytes)
-		}) { (e) in
-			error(e)
-		}
+		}, errorHandler: { err in
+			error(err)
+		})
 	}
-	
-	static func transactionsToApprove(nodeAddress: String, depth: Int = 10, reference: String? = nil, _ success: @escaping (_ txs: (trunkTx: String, branchTx: String)) -> Void, _ error: @escaping (Error) -> Void) {
-		
+
+	static func transactionsToApprove(
+		nodeAddress: String,
+		depth: Int = 10,
+		reference: String? = nil,
+		_ success: @escaping (_ txs: (trunkTx: String, branchTx: String)) -> Void,
+		_ error: @escaping (Error) -> Void) {
+
 		var data = command(withString: "getTransactionsToApprove")
 		data["depth"] = depth
-		if let r = reference {
-			data["reference"] = r
+		if let reference = reference {
+			data["reference"] = reference
 		}
-		service.POST(data: data, destination: nodeAddress, timeout: 3600, successHandler: { (r) in
-			guard let dict = r.jsonToObject() as? [String: Any] else {
+		service.POST(data: data, destination: nodeAddress, timeout: 3600, successHandler: { result in
+			guard let dict = result.jsonToObject() as? [String: Any] else {
 				error(IotaAPIError("Error converting JSON"))
 				return
 			}
@@ -156,157 +181,167 @@ class IotaAPIService: IotaAPIServices {
 				return
 			}
 			success((trunkTx, branchTx))
-		}) { (e) in
-			error(e)
-		}
+		}, errorHandler: { err in
+			error(err)
+		})
 	}
-	
+
 	static func broadcastTransactions(nodeAddress: String, trytes: [String], _ success: @escaping (() -> Void), _ error: @escaping (Error) -> Void) {
-		
+
 		var data = command(withString: "broadcastTransactions")
 		data["trytes"] = trytes
-		service.POST(data: data, destination: nodeAddress, timeout: defaultTimeout, successHandler: { (r) in
-			guard let dict = r.jsonToObject() as? [String: Any] else {
+		service.POST(data: data, destination: nodeAddress, timeout: defaultTimeout, successHandler: { result in
+			guard let dict = result.jsonToObject() as? [String: Any] else {
 				error(IotaAPIError("Error converting JSON"))
 				return
 			}
-			
-			if let e = dict["error"] as? String {
-				error(IotaAPIError(e))
+
+			if let err = dict["error"] as? String {
+				error(IotaAPIError(err))
 				return
 			}
-			if let e = dict["exception"] as? String {
-				error(IotaAPIError(e))
+			if let err = dict["exception"] as? String {
+				error(IotaAPIError(err))
 				return
 			}
 			success()
-			
-		}) { (e) in
-			error(e)
-		}
+
+		}, errorHandler: { err in
+			error(err)
+		})
 	}
-	
+
 	static func storeTransactions(nodeAddress: String, trytes: [String], _ success: @escaping (() -> Void), _ error: @escaping (Error) -> Void) {
-		
+
 		var data = command(withString: "storeTransactions")
 		data["trytes"] = trytes
-		service.POST(data: data, destination: nodeAddress, timeout: defaultTimeout, successHandler: { (r) in
-			guard let dict = r.jsonToObject() as? [String: Any] else {
+		service.POST(data: data, destination: nodeAddress, timeout: defaultTimeout, successHandler: { result in
+			guard let dict = result.jsonToObject() as? [String: Any] else {
 				error(IotaAPIError("Error converting JSON"))
 				return
 			}
-			if let e = dict["error"] as? String {
-				error(IotaAPIError(e))
+			if let err = dict["error"] as? String {
+				error(IotaAPIError(err))
 				return
 			}
-			if let e = dict["exception"] as? String {
-				error(IotaAPIError(e))
+			if let err = dict["exception"] as? String {
+				error(IotaAPIError(err))
 				return
 			}
 			success()
-		}) { (e) in
-			error(e)
-		}
+		}, errorHandler: { err in
+			error(err)
+		})
 	}
-	
-	static func inclusionStates(nodeAddress: String, hashes: [String], tips: [String], _ success: @escaping (([Bool]) -> Void), _ error: @escaping (Error) -> Void) {
-		
+
+	static func inclusionStates(
+		nodeAddress: String,
+		hashes: [String],
+		tips: [String],
+		_ success: @escaping (([Bool]) -> Void),
+		_ error: @escaping (Error) -> Void) {
+
 		var data = command(withString: "getInclusionStates")
 		data["transactions"] = hashes
 		data["tips"] = tips
-		
-		service.POST(data: data, destination: nodeAddress, timeout: defaultTimeout, successHandler: { (r) in
-			guard let dict = r.jsonToObject() as? [String: Any] else {
+
+		service.POST(data: data, destination: nodeAddress, timeout: defaultTimeout, successHandler: { result in
+			guard let dict = result.jsonToObject() as? [String: Any] else {
 				error(IotaAPIError("Error converting JSON"))
 				return
 			}
-			if let e = dict["error"] as? String {
-				error(IotaAPIError(e))
+			if let err = dict["error"] as? String {
+				error(IotaAPIError(err))
 				return
 			}
-			if let e = dict["exception"] as? String {
-				error(IotaAPIError(e))
+			if let err = dict["exception"] as? String {
+				error(IotaAPIError(err))
 				return
 			}
-			
+
 			if let states = dict["states"] as? [Bool] {
 				success(states)
 				return
 			}
 			error(IotaAPIError("Malformed JSON"))
-		}) { (e) in
-			error(e)
-		}
+		}, errorHandler: { err in
+			error(err)
+		})
 	}
-	
+
 	static func checkConsistency(nodeAddress: String, hashes: [String], _ success: @escaping ((Bool) -> Void), _ error: @escaping (Error) -> Void) {
-		
+
 		var data = command(withString: "checkConsistency")
 		data["tails"] = hashes
-		
-		service.POST(data: data, destination: nodeAddress, timeout: defaultTimeout, successHandler: { (r) in
-			guard let dict = r.jsonToObject() as? [String: Any] else {
+
+		service.POST(data: data, destination: nodeAddress, timeout: defaultTimeout, successHandler: { result in
+			guard let dict = result.jsonToObject() as? [String: Any] else {
 				error(IotaAPIError("Error converting JSON"))
 				return
 			}
-			if let e = dict["error"] as? String {
-				error(IotaAPIError(e))
+			if let err = dict["error"] as? String {
+				error(IotaAPIError(err))
 				return
 			}
-			if let e = dict["exception"] as? String {
-				error(IotaAPIError(e))
+			if let err = dict["exception"] as? String {
+				error(IotaAPIError(err))
 				return
 			}
 			if let state = dict["state"] as? Int {
 				success(state == 1 ? true : false)
 				return
 			}
-			
 			if let state = dict["state"] as? Bool {
 				success(state)
 				return
 			}
 			error(IotaAPIError("Malformed JSON"))
-		}) { (e) in
-			error(e)
-		}
+		}, errorHandler: { err in
+			error(err)
+		})
 	}
-	
-	static func latestInclusionStates(nodeAddress: String, hashes: [String], _ success: @escaping (([Bool]) -> Void), _ error: @escaping (Error) -> Void) {
-		
+
+	static func latestInclusionStates(
+		nodeAddress: String,
+		hashes: [String],
+		_ success: @escaping (([Bool]) -> Void),
+		_ error: @escaping (Error) -> Void) {
+
 		self.nodeInfo(nodeAddress: nodeAddress, { (nodeInfo) in
 			guard let milestone = nodeInfo["latestSolidSubtangleMilestone"] as? String else { error(IotaAPIError("Error getting latest milestone")); return }
-			
 			self.inclusionStates(nodeAddress: nodeAddress, hashes: hashes, tips: [milestone], success, error)
 		}, error)
 	}
-	
-	static func wereAddressesSpentFrom(nodeAddress: String, addresses: [String], _ success: @escaping (([Bool]) -> Void), _ error: @escaping (Error) -> Void) {
-		
+
+	static func wereAddressesSpentFrom(
+		nodeAddress: String,
+		addresses: [String],
+		_ success: @escaping (([Bool]) -> Void),
+		_ error: @escaping (Error) -> Void) {
+
 		var data = command(withString: "wereAddressesSpentFrom")
 		data["addresses"] = addresses
-		
-		service.POST(data: data, destination: nodeAddress, timeout: defaultTimeout, successHandler: { (r) in
-			guard let dict = r.jsonToObject() as? [String: Any] else {
+
+		service.POST(data: data, destination: nodeAddress, timeout: defaultTimeout, successHandler: { result in
+			guard let dict = result.jsonToObject() as? [String: Any] else {
 				error(IotaAPIError("Error converting JSON"))
 				return
 			}
-			if let e = dict["error"] as? String {
-				error(IotaAPIError(e))
+			if let err = dict["error"] as? String {
+				error(IotaAPIError(err))
 				return
 			}
-			if let e = dict["exception"] as? String {
-				error(IotaAPIError(e))
+			if let err = dict["exception"] as? String {
+				error(IotaAPIError(err))
 				return
 			}
-			
 			if let states = dict["states"] as? [Bool] {
 				success(states)
 				return
 			}
 			error(IotaAPIError("Malformed JSON"))
-		}) { (e) in
-			error(e)
-		}
+		}, errorHandler: { err in
+			error(err)
+		})
 	}
 }
